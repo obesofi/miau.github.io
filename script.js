745440a6367caec149cee7586242a6f54188a6a7
obesofi <118153754+obesofi@users.noreply.github.com>
2022-11-14 02:26:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+// inicializacion de variables
+let tarjetasDestapadas = 0;
+let tarjeta1 = null;
+let tarjeta2 = null;
+let primerResultado = null;
+let segundoResultado = null;
+let movimientos = 0;
+let aciertos = 0;
+let temporizador = false;
+let timer = 30;
+let tiempoRegresivo = null;
+let timerInicial = timer;
+
+let winAudio = new Audio('./sonidos/win.wav');
+let loseAudio = new Audio('./sonidos/lose.wav');
+let clickAudio = new Audio('./sonidos/click.wav');
+let rightAudio = new Audio('./sonidos/right.wav');
+let wrongAudio = new Audio('./sonidos/wrong.wav');
+
+// apuntando a documento html
+let mostrarMovimientos = document.getElementById('movimientos');
+let mostrarAciertos = document.getElementById('aciertos');
+let mostrarTiempo = document.getElementById('t-restante');
+
+// generacion de numeros aleatorios
+let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+numeros = numeros.sort(()=>{return Math.random()-0.5});
+console.log(numeros);
+
+// funciones
+function contarTiempo(){
+    tiempoRegresivo = setInterval(() => {
+    timer--;
+    mostrarTiempo.innerHTML = `Tiempo: ${timer} segundos`;
+    if(timer == 0){
+        clearInterval(tiempoRegresivo);
+        bloquearTarjetas();
+        loseAudio.play();
+    }
+}, 1000);
+}
+
+function bloquearTarjetas(){
+
+    for(let i=0;i<=15;i++){
+        let tarjetaBloqueada = document.getElementById(i);
+        tarjetaBloqueada.innerHTML = `<img src ="./img/${numeros[i]}.png" alt"">`;
+        //tarjetaBloqueada.innerHTML = numeros[i];
+        tarjetaBloqueada.disabled = true;
+    }
+    mostrarTiempo.innerHTML = `Perdiste! demasiado lento :p`;
+    
+}
+
+// funcion principal
+function girar(Id){
+    if(temporizador == false){
+        contarTiempo();
+        temporizador = true;
+    }
+
+    tarjetasDestapadas++;
+    console.log(tarjetasDestapadas);
+
+    if(tarjetasDestapadas == 1){
+        //mostrar el primer numero
+        tarjeta1 = document.getElementById(Id);
+        primerResultado = numeros[Id];
+        tarjeta1.innerHTML= `<img src ="./img/${primerResultado}.png" alt"">`;
+
+        clickAudio.play();
+        // deshabilitar el primer boton
+        tarjeta1.disabled = true; 
+    } else if(tarjetasDestapadas == 2){
+        //mostrar segundo numero
+        tarjeta2 = document.getElementById(Id);
+        segundoResultado = numeros[Id];
+        tarjeta2.innerHTML= `<img src ="./img/${segundoResultado}.png" alt"">`;
+
+        // deshabilitar segundo boton
+        tarjeta2.disabled = true;
+
+        // incremetar movimientos
+         movimientos ++;
+         mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
+
+        if(primerResultado == segundoResultado){
+            rightAudio.play();
+            //encerar contador tarjetas destapadas
+            tarjetasDestapadas = 0;
+
+            //aumentar aciertos
+            aciertos++;
+            console.log(aciertos);
+            mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`;
+            
+            if(aciertos == 8){
+                clearInterval(tiempoRegresivo);
+                mostrarAciertos.innerHTML = `Aciertos: ${aciertos}😱`;
+                mostrarTiempo.innerHTML = `Fantastico! Solo demoraste ${timerInicial - timer} segundos 😋`;
+                mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}🤟😎`;
+                winAudio.play();
+            }
+            
+            
+        }else {
+            wrongAudio.play();
+            // mostrar momentaneamente los valores y volver a tapar
+            setTimeout(()=>{
+                tarjeta1.innerHTML= ' ';
+                tarjeta2.innerHTML =' ';
+                tarjeta1.disabled = false;
+                tarjeta2.disabled = false;
+                tarjetasDestapadas = 0;
+            },800);
+        }
+    }
+}
